57e18ef6f889c86e25a8ca0e221efff82d70e47e
oliynykroma <oliynykroma@gmail.com>
2022-04-26 10:29:13 +0300

    added markup for search, added axios

--- vue-study/src/store/index.ts
+++ vue-study/src/store/index.ts
@@ -4,9 +4,20 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    users: null,
+    userData: null
+  },
+  getters: {
+    USERS: state => {
+      return state.users;
+    },
+    USERDATA: state => {
+      return state.userData;
+    },
+    mutations: {
+      
+    },
+    actions: {},
+    modules: {},
+  });
